c3029720ebe362977aec8c17bc069cbcf071552a
audreystjohn <astjohn@mtholyoke.edu>
2020-05-04 20:15:18 -0400

    Update prompts.js

--- discomfort-prompts/prompts.js
+++ discomfort-prompts/prompts.js
@@ -101,8 +101,36 @@ var resetPrompt = function(){
 
 };
 
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+var shuffle = function (array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
+
 // addTaskBtn.addEventListener("click", addTask);
 inComfortBtn.addEventListener("click", assignComfort);
 outComfortBtn.addEventListener("click", assignDisComfort);
 neutralBtn.addEventListener("click", assignNeutral);
+prompts = shuffle( prompts );
 createNewPrompt();
